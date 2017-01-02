817ae8e6dcfc1d7180f2da77e3566c974785137f
Matthew Krueger <matthapkidokarate@gmail.com>
2017-01-02 14:48:35 -0500

    added a showing errors

--- html/index.php
+++ html/index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
   <?php
-    error_reporting(-1);
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
     require_once("/var/www/html/func/core/init.php");
   ?>
 	<meta charset="utf-8">
