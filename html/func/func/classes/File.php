2bb1753f0d6361a0a26fedffb91dd3612e837817
Matthew Krueger <matthapkidokarate@gmail.com>
2017-01-02 19:58:51 +0000

    Removed Func Folder for Update

--- html/func/func/classes/File.php
+++
@@ -1,9 +0,0 @@
-<?php
-class File{
-  /**
-  * File path, absolute to website. Gets data from config on path to server.
-  */
-  private function __construct($){
-
-  }
-}
--- html/func/includes/errors/404user.php
+++
@@ -1,1 +0,0 @@
-That page cannot be found
