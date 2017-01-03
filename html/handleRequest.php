f407eca82454b07e5862b633b6c137dd083f1adb
Matthew Krueger <matthapkidokarate@gmail.com>
2017-01-02 19:36:28 -0500

    sleep

--- html/handleRequest.php
+++ html/handleRequest.php
@@ -3,4 +3,5 @@
   ini_set('display_errors', 1);
 file_put_contents("/var/www/php_shell.sh", "  #!/bin/sh
   smfplay -d 20:0 /var/www/midi/" . $_GET['file']);
+  sleep(1);
   exec("/var/www/php_root");
