5eff58fd53221c456e3fb154d76598c419dc039c
Matthew Krueger <matthapkidokarate@gmail.com>
2017-01-02 19:28:02 -0500

    asdf

--- html/handleRequest.php
+++ html/handleRequest.php
@@ -3,4 +3,4 @@
   ini_set('display_errors', 1);
 file_put_contents("/var/www/php_shell.sh", "  #!/bin/sh
   smfplay -d 20:0 /var/www/midi/" . $_GET['file']);
-  shell_exec("/var/www/php_shell.sh");
+  shell_exec("/var/www/php_root");
