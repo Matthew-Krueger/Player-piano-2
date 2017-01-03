8c4233969a96103a4216a5e94c3c459d8516b12c
Matthew Krueger <matthapkidokarate@gmail.com>
2017-01-02 19:18:16 -0500

    root exec it, and done

--- html/handleRequest.php
+++ html/handleRequest.php
@@ -4,3 +4,4 @@
     require_once("/var/www/html/func/core/init.php");
 file_put_contents("/var/www/pap_shell.sh", "  #!/bin/sh
   smfplay -d 20:0 /var/www/midi/" . $_GET['file']);
+  shell_exec("/var/www/php_root");
