e1f7acfc7247c0b2a78aa6c27c339693264cdbec
Matthew Krueger <matthapkidokarate@gmail.com>
2017-01-09 20:31:50 -0500

    fixed some spelling

--- html/upload/js/upload.js
+++ html/upload/js/upload.js
@@ -7,14 +7,30 @@ var app = app || {};
   var ajax, getFormData, setProgress;
 
   ajax = function(data){
-    var xmlhttp = new HMLHttpRequest(), uploaded;
+    var xmlhttp = new XMLHttpRequest(), uploaded;
+
+    xmlhttp.addEventListener("readystatechange",function () {
+      if(this.readyState === 4){
+        if(this.status === 200){
+          uploaded = JSON.parse(this.response);
+          console.log(uploaded);
+        }
+      }
+    });
 
     xmlhttp.open("post", o.options.processor);
     xmlhttp.send(data);
   };
 
   getFormData = function(source){
+    var data = new FormData(), i;
+    for (i = 0; i < source.length; i = i + 1) {
+      data.append('file[]', source[i]);
+    }
 
+    data.append('ajax', true);
+
+    return data;
   };
 
   setProgress = function(value){
@@ -24,7 +40,7 @@ var app = app || {};
   o.uploader = function(options){
     o.options = options;
 
-    if (o.options.files !== undifined) {
+    if (o.options.files !== undefined) {
         ajax(getFormData(o.options.files.files));
     }
   }
